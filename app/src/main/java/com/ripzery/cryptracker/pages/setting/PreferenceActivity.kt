0c4121aacf0f3c727853b8f17a63cafb733e77cc
Phuchit Sirimongkolsathien <ripzery@gmail.com>
2017-10-24 13:04:23 +0700

    Add toolbar to preference activity

--- app/src/main/java/com/ripzery/cryptracker/pages/setting/PreferenceActivity.kt
+++ app/src/main/java/com/ripzery/cryptracker/pages/setting/PreferenceActivity.kt
@@ -3,6 +3,7 @@ package com.ripzery.cryptracker.pages.setting
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import com.ripzery.cryptracker.R
 import com.ripzery.cryptracker.pages.setting.manage_crypto.ManageCryptoActivity
 import kotlinx.android.synthetic.main.activity_preference.*
@@ -15,7 +16,17 @@ class PreferenceActivity : AppCompatActivity() {
         initInstance()
     }
 
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            android.R.id.home -> finish()
+        }
+        return true
+    }
+
     private fun initInstance() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Preference"
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         layoutManageCurrency.setOnClickListener { startActivity(Intent(this, ManageCryptoActivity::class.java)) }
     }
 }

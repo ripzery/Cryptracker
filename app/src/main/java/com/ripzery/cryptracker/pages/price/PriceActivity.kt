6362bbb5276865eac09ca316d37f0690ccac2e36
Phuchit Sirimongkolsathien <ripzery@gmail.com>
2017-10-28 10:59:12 +0700

    Add animation setting icon

--- app/src/main/java/com/ripzery/cryptracker/pages/price/PriceActivity.kt
+++ app/src/main/java/com/ripzery/cryptracker/pages/price/PriceActivity.kt
@@ -8,9 +8,15 @@ import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
+import android.support.v4.content.ContextCompat
+import android.support.v4.view.animation.FastOutSlowInInterpolator
 import android.support.v7.app.AppCompatActivity
+import android.util.TypedValue
 import android.view.Menu
 import android.view.MenuItem
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
+import android.widget.ImageView
 import com.ripzery.cryptracker.R
 import com.ripzery.cryptracker.network.DataSource
 import com.ripzery.cryptracker.pages.setting.PreferenceActivity
@@ -25,6 +31,7 @@ class PriceActivity : AppCompatActivity() {
     private var mCryptocurrencyList: MutableList<String> = mutableListOf("omisego", "everex", "ethereum", "bitcoin")
     private lateinit var mPagerAdapter: PricePagerAdapter
     private val MAX_ITEMS_TAB_LAYOUT_FIXED = 4
+    private var mFirstTime = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,8 +54,32 @@ class PriceActivity : AppCompatActivity() {
         }, 700)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_price, menu)
+        val outValue = TypedValue()
+        theme.resolveAttribute(android.R.attr.selectableItemBackgroundBorderless, outValue, true)
+
+        val settingIcon = menu.findItem(R.id.setting).actionView as ImageView
+        settingIcon.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_settings_24dp))
+        settingIcon.isClickable = true
+        settingIcon.setBackgroundResource(outValue.resourceId)
+
+        val rotateAnim = RotateAnimation(0f, 180f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+        rotateAnim.duration = 500
+        rotateAnim.interpolator = FastOutSlowInInterpolator()
+        appBar.addOnOffsetChangedListener { _, verticalOffset ->
+            if (verticalOffset == 0 && mFirstTime) {
+                settingIcon.startAnimation(rotateAnim)
+                mFirstTime = false
+            } else if (verticalOffset < -50) {
+                mFirstTime = true
+            }
+        }
+
+        settingIcon.setOnClickListener {
+            mFirstTime = false
+            startActivityForResult(Intent(this, PreferenceActivity::class.java), 100)
+        }
         return true
     }
 

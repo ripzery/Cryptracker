1fbc45203ce30486494e334c3626a08a089243f3
Phuchit Sirimongkolsathien <ripzery@gmail.com>
2017-10-29 16:02:49 +0700

    Move business logic in PriceFragment to PriceViewModel

--- app/src/main/java/com/ripzery/cryptracker/pages/price/PriceFragment.kt
+++ app/src/main/java/com/ripzery/cryptracker/pages/price/PriceFragment.kt
@@ -4,16 +4,12 @@ import android.annotation.SuppressLint
 import android.arch.lifecycle.Observer
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ripzery.cryptracker.R
 import com.ripzery.cryptracker.SpringHelper
 import com.ripzery.cryptracker.extensions.getViewModel
-import com.ripzery.cryptracker.extensions.to2Precision
-import com.ripzery.cryptracker.network.DataSource
-import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.fragment_price.*
 
 /**
@@ -24,14 +20,11 @@ class PriceFragment : Fragment() {
 
     /** Variable zone **/
     private lateinit var mCryptocurrency: String
-    private val mDisposableList: CompositeDisposable = CompositeDisposable()
-    private val mHandleAPIError: (Throwable) -> Unit = { error -> Log.d("Error", error.message) }
     private lateinit var mCurrencyTop: String
     private lateinit var mCurrencyBottom: String
     private lateinit var mSpringHelper: SpringHelper<View, View>
-    private val USD_TO_THB = 33.23
-    private val THB_TO_USD = 0.03
-    private val mViewModel by lazy { getViewModel(PriceViewModel::class.java) }
+
+    private val mViewModel by lazy { getViewModel(PriceViewModel::class.java).apply { init(mCryptocurrency, mCurrencyTop, mCurrencyBottom) } }
 
 
     /** Static method zone **/
@@ -58,28 +51,15 @@ class PriceFragment : Fragment() {
         mCryptocurrency = arguments.getString(ARG_1)
         mCurrencyTop = arguments.getString(ARG_2)
         mCurrencyBottom = arguments.getString(ARG_3)
-//            mCryptocurrency = savedInstanceState.getString("cryptocurrency")
-//            mCurrencyTop = savedInstanceState.getString("currencyTop")
-//            mCurrencyBottom = savedInstanceState.getString("currencyBottom")
-
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val rootView: View = inflater!!.inflate(R.layout.fragment_price, container, false)
-
-        return rootView
+        return inflater!!.inflate(R.layout.fragment_price, container, false)
     }
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initInstance()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        outState.putString("cryptocurrency", mCryptocurrency)
-        outState.putString("currencyTop", mCurrencyTop)
-        outState.putString("currencyBottom", mCurrencyBottom)
-        super.onSaveInstanceState(outState)
     }
 
     override fun onStart() {
@@ -102,19 +82,8 @@ class PriceFragment : Fragment() {
         tvBx.scaleX = 0.8f
         tvBx.scaleY = 0.8f
         tvCoinMarketCap.translationY = -100f
-
-        if (mCurrencyTop == "usd") {
-            tvCoinMarketCap.text = coinMarketCap
-        } else if (mCurrencyTop == "thb") {
-            tvCoinMarketCap.text = (coinMarketCap.toFloat() * USD_TO_THB).to2Precision()
-        }
-
-        if (mCurrencyBottom == "usd") {
-            tvBx.text = (bx.toFloat() * THB_TO_USD).to2Precision()
-        } else if (mCurrencyBottom == "thb") {
-            tvBx.text = bx
-        }
-
+        tvCoinMarketCap.text = coinMarketCap
+        tvBx.text = bx
         mSpringHelper.start()
     }
 }
--- app/src/main/java/com/ripzery/cryptracker/pages/price/PriceViewModel.kt
+++ app/src/main/java/com/ripzery/cryptracker/pages/price/PriceViewModel.kt
@@ -5,6 +5,7 @@ import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.MutableLiveData
 import android.util.Log
 import com.ripzery.cryptracker.extensions.TAG
+import com.ripzery.cryptracker.extensions.to2Precision
 import com.ripzery.cryptracker.network.DataSource
 import io.reactivex.disposables.CompositeDisposable
 
@@ -15,10 +16,28 @@ class PriceViewModel(context: Application) : AndroidViewModel(context) {
     private val mHandleAPIError: (Throwable) -> Unit = { error -> Log.d("Error", error.message) }
     private val mDisposableList: CompositeDisposable = CompositeDisposable()
     private val mLiveData: MutableLiveData<Pair<String, String>> = MutableLiveData()
+    private val USD_TO_THB = 33.23
+    private val THB_TO_USD = 0.03
+    private lateinit var mCurrencyTop: String
+    private lateinit var mCurrencyButtom: String
+    private lateinit var mCryptocurrency: String
+
+    fun init(cryptocurrency: String, currencyTop: String, currencyBottom: String) {
+        this.mCryptocurrency = cryptocurrency
+        this.mCurrencyTop = currencyTop
+        this.mCurrencyButtom = currencyBottom
+    }
 
     fun pollingPrice(cryptocurrency: String): MutableLiveData<Pair<String, String>> {
         val d = DataSource.getPriceForInterval(cryptocurrency, 5, mHandleAPIError) { coinMarketCap, bx ->
-            mLiveData.value = Pair(coinMarketCap, bx)
+            val price: Pair<String, String> = when {
+                mCurrencyTop == "usd" && mCurrencyButtom == "thb" -> Pair(coinMarketCap, bx)
+                mCurrencyTop == "usd" && mCurrencyButtom == "usd" -> Pair(coinMarketCap, (bx.toFloat() * THB_TO_USD).to2Precision())
+                mCurrencyTop == "thb" && mCurrencyButtom == "thb" -> Pair((coinMarketCap.toFloat() * USD_TO_THB).to2Precision(), bx)
+                mCurrencyTop == "thb" && mCurrencyButtom == "usd" -> Pair((coinMarketCap.toFloat() * USD_TO_THB).to2Precision(), (bx.toFloat() * THB_TO_USD).to2Precision())
+                else -> throw UnsupportedOperationException("Unsupported currency!")
+            }
+            mLiveData.value = price
         }
         mDisposableList.add(d)
         return mLiveData

e5ea8034eb5bfe995d69358a391acb75decb8d96
Phuchit Sirimongkolsathien <ripzery@gmail.com>
2017-10-31 16:55:25 +0700

    Fix buddybuild script

--- buddybuild_finally.sh
+++ buddybuild_finally.sh
@@ -1,1 +1,1 @@
-./gradlew -PPFirebaseServiceAccountFilePath=$BUDDYBUILD_SECURE_FILES/cryptracker-firebase-crashreporting-r3gco-3747615314.json :app:firebaseUploadReleaseProguardMapping --stacktrace
+./gradlew -PFirebaseServiceAccountFilePath=$BUDDYBUILD_SECURE_FILES/cryptracker-firebase-crashreporting-r3gco-3747615314.json :app:firebaseUploadReleaseProguardMapping --stacktrace
